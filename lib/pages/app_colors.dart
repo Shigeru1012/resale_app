ce28b7f8d321b213bed66391f80289b05b18bd3f
Ochiai Shigeru <72843750+Shigeru1012@users.noreply.github.com>
2023-01-10 14:03:15 +0700

    Update app_colors.dart

--- lib/pages/app_colors.dart
+++ lib/pages/app_colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color lightBlue = Color(0xff0077DC);
+  static const Color lightBlue = Color(0xff94a2f2);
+  static const Color lightPurple = Color(0xffea91a1);
 }
