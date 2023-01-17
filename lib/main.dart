8442623dbba06e457ee6508070ef32d0afe3566d
Ochiai Shigeru <72843750+Shigeru1012@users.noreply.github.com>
2023-01-17 16:28:58 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:resale_app/pages/home_page.dart';
+import 'package:resale_app/pages/message_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +13,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'resale app',
-      home: HomePage(),
-    );
+    return MultiProvider(
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'resale app',
+          home: HomePage(),
+        ),
+        providers: [
+          ChangeNotifierProvider(create: (_) => MessageProvider())
+        ]);
   }
 }
 
